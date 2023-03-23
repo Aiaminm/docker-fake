182b68170b63b28dac104fd2e7b02fa7c50f039d
Aiaminm <70017178+Aiaminm@users.noreply.github.com>
2023-03-23 16:42:32 +0800

    Update installstep.sh

--- installstep.sh
+++ installstep.sh
@@ -4,7 +4,7 @@ apt install software-properties-common -y
 
 
 apt install -y language-pack-zh-hans language-pack-zh-hans-base language-pack-zh-hant language-pack-zh-hant-base language-pack-ja
-apt install nano sudo wget curl screen   -y 
+apt install nano sudo wget curl screen git   -y 
 
 add-apt-repository ppa:qbittorrent-team/qbittorrent-stable 
 apt update -y
