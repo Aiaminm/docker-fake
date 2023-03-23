10f6194d0ad2e338f92c2a15bfa062bf4fa4481b
Aiaminm <70017178+Aiaminm@users.noreply.github.com>
2023-03-23 15:32:54 +0800

    Update installstep.sh

--- installstep.sh
+++ installstep.sh
@@ -1,7 +1,9 @@
 apt-fast update -y
 
 apt install software-properties-common -y
-
+add-apt-repository ppa:apt-fast/stable
+apt-get update
+apt-get -y install apt-fast
 
 apt-fast install -y language-pack-zh-hans language-pack-zh-hans-base language-pack-zh-hant language-pack-zh-hant-base language-pack-ja
 apt-fast install nano sudo wget curl screen nodejs npm  -y 
